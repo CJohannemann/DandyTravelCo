b7a2607485a23d913e0e594c68f39baee1df33fb
Chris Johannemann <cjjohannemann@gmail.com>
2023-12-21 21:34:57 -0500

    added blog

--- src/main/frontend/src/pages/blog/Blog.tsx
+++ src/main/frontend/src/pages/blog/Blog.tsx
@@ -4,7 +4,16 @@ const Blog = () => {
     return (
         <div>
             <br/>
-            <p>This is the blog page.. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+            <p><b>Intro Blog Post</b>
+                Hello and welcome! I am so excited to write this and introduce you to the fun things that are to come. My name is Lauren, and I’m married to Chris, and we have 3 kids, Briella, Eli, and Ava.
+                Our family discovered our love for camping in 2020 when the world was shut down. Like many others, we were searching for things to do and thought it was the perfect time to purchase our first travel trailer. Since then, we have fallen in love with camping and the memories that we have been creating with our kids.
+                I am excited to start this new journey and to share our knowledge and experiences with others that also love camping!
+                <b>What to Expect</b>
+                Our blog will contain tips and tricks for RV travel, including both specific destinations and general travel advice. This is of course free to anyone and everyone!
+                <b><i>Coming Soon…</i></b>
+                In the future, we will be offering travel planning services for RV travel across the United States, with our specialty being the East Coast. We will offer suggested itineraries but also offer the flexibility for custom itineraries. We will take care of the RV rental (if needed), booking of campsites, provide RV-friendly routes, tips & tricks, and suggested activities.
+                Visit our website to learn more about us and follow us or subscribe to stay in the loop with our future posts!
+            </p>
         </div>
     );
 };
