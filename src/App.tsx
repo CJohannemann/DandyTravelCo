f354d16b822ef01019f01b74d9daa25d66708c3c
cjohannemann <cjjohannemann@gmail.com>
2024-02-19 23:07:18 -0500

    remove import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import {Route, Routes} from "react-router";
-import {HashRouter as Router, Routes, Route, BrowserRouter} from "react-router-dom";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
 import HomePage from "./main/frontend/src/pages/home";
 import Navbar from "./main/frontend/src/pages/components/navBar";
 import Pricing from "./main/frontend/src/pages/pricing/Pricing";
@@ -9,7 +9,6 @@ import Planning from "./main/frontend/src/pages/planning/Planning";
 function App() {
     return (
         <BrowserRouter>
-        // <Router>
             <Navbar/>
             <Routes>
                 <Route path='/' element={<HomePage/>}/>
@@ -17,7 +16,6 @@ function App() {
                 <Route path='/plan-your-trip' element={<Planning/>}/>
                 {/*<Route path="/Blog" element={<Blog/>}/>*/}
             </Routes>
-        // </Router>
         </BrowserRouter>
     );
 }
