19ca7c02ee6511b84d184a39baf3f95defdcd6bc
Chris Johannemann <cjjohannemann@gmail.com>
2024-04-05 23:25:17 -0400

    northeast kinda done

--- src/main/frontend/src/pages/itineraries/northEast/NorthEastItinerary.tsx
+++ src/main/frontend/src/pages/itineraries/northEast/NorthEastItinerary.tsx
@@ -1,6 +1,4 @@
-import { url } from "inspector";
 import './NorthEastItinerary.scss'
-import Northeast from './img/Northeast.png'
 
 
 const NorthEastItinerary = () => {
@@ -8,12 +6,36 @@ const NorthEastItinerary = () => {
         <>
             <div style={{ paddingTop: "150px" }}></div>
             <div className="box">
-                <div className="north-east-itin-img">
-                    <div className="chr">
-                        <h2 id="chr-txt">Northeast</h2>
-                    </div>
+                <p id="coverPhoto"></p>
+            </div>
+
+            <div className="verbiage">
+                <div className="content-container">
+                    <h5>Trip Details</h5>
+                    <p>Duration: 18 nights <br />
+                        Starting/Ending City: Northern Kentucky (customizable) <br />
+                        <i>Pricing varies based on customization. <span>See </span>
+                            <a href="/pricing">
+                                <u>pricing page</u>
+                            </a>
+                            <span> for details.</span>
+                        </i>
+                    </p>
+                    <p></p>
+                </div>
+
+                <div>
+                    <h5>Things we love on this route:</h5>
+                    <p>
+                        • Explore all the northeast hast to offer in one trip
+                    </p>
                 </div>
             </div>
+
+            <div className="book-btn-container">
+                <a href="/Plan-Your-Trip" id="book-btn">Book Now</a>
+            </div>
+
             <div className="north-east-route-img-conatiner">
                 <p id="north-east-route-img"></p>
             </div>
